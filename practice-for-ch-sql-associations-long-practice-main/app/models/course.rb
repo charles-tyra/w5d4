d52d024ca34719dba9cb5d09c687dcda5203fc2f
Charles Tyra <charlestyra@MacBook-Pro.local>
2022-12-08 15:46:48 -0500

    Course and Student enrollment through methods, also creating relations with enrollment methods.

--- practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
+++ practice-for-ch-sql-associations-long-practice-main/app/models/course.rb
@@ -10,4 +10,15 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+   validates :name, presence: true
+   validates :instructor_id, presence: true
+
+   has_many :enrollments,
+      primary_key: :id,
+      foreign_key: :course_id,
+      class_name: :Enrollment
+
+   has_many :enrolled_student,
+      through: :enrollments,
+      source: :student
 end
